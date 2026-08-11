5726fe5d7c90ef7d78530712b74a5bc37746bb65
Rômulo Fernando <fssromulo@gmail.com>
2026-08-11 13:44:41 +0000

    Fix migration endpoint bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,34 +3,45 @@ import { join } from "node:path";
 import database from "infra/database";
 
 export default async function migrations(request, response) {
-  const dbClient = await database.getNewCliente();
-  const defaultMigrationsOptions = {
-    dbClient: dbClient,
-    databaseUrl: process.env.DATABASE_URL,
-    dryRun: true,
-    dir: join("infra", "migrations"), // usa esse método para evitar problemas com caracteres especiais no linux, windows etc.
-    direction: "up",
-    verbose: true,
-    migrationsTable: "pgmigrations",
-  };
-
-  if (request.method === "GET") {
-    const pendingMigrations = await migrationRunner({
-      ...defaultMigrationsOptions,
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method "${request.method}" not allowed`,
     });
-    await dbClient.end();
-    return response.status(200).json(pendingMigrations);
   }
 
-  if (request.method === "POST") {
-    const migratedMigrations = await migrationRunner({
-      ...defaultMigrationsOptions,
-      dryRun: false,
-    });
-    await dbClient.end();
-    const statusCode = migratedMigrations.length > 0 ? 201 : 200;
-    return response.status(statusCode).json(migratedMigrations);
-  }
+  let dbClient;
+  try {
+    dbClient = await database.getNewCliente();
+    const defaultMigrationsOptions = {
+      dbClient,
+      databaseUrl: process.env.DATABASE_URL,
+      dryRun: true,
+      dir: join("infra", "migrations"), // usa esse método para evitar problemas com caracteres especiais no linux, windows etc.
+      direction: "up",
+      verbose: true,
+      migrationsTable: "pgmigrations",
+    };
 
-  return response.status(405).end();
+    if (request.method === "GET") {
+      const pendingMigrations = await migrationRunner({
+        ...defaultMigrationsOptions,
+      });
+      return response.status(200).json(pendingMigrations);
+    }
+
+    if (request.method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationsOptions,
+        dryRun: false,
+      });
+      const statusCode = migratedMigrations.length > 0 ? 201 : 200;
+      return response.status(statusCode).json(migratedMigrations);
+    }
+  } catch (error) {
+    console.error("[migrations/index.js] - Error:", error);
+    throw error;
+  } finally {
+    await dbClient.end();
+  }
 }
